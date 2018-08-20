35c000401f5e25b347f9e83dc6d479f45c4d72e6
Aang23 <qwerty15@gmx.fr>
2018-08-20 08:34:14 +0200

    Started command implementation

---
+++ src/main/java/com/aang23/realserene/commands/RealSereneCommand.java
@@ -0,0 +1,31 @@
+package com.aang23.realserene.commands;
+
+import net.minecraft.command.CommandBase;
+import net.minecraft.command.CommandException;
+import net.minecraft.command.ICommandSender;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.TextComponentString;
+
+
+public class RealSereneCommand extends CommandBase {
+	
+	@Override
+	public String getName()
+	{
+	return "realserene";
+	}
+
+	@Override
+	public String getUsage(ICommandSender icommandsender)
+	{
+	return "/realserene <subcommand>";
+	}
+
+	@Override
+	public void execute(MinecraftServer server, ICommandSender sender, String[] args) throws CommandException {
+		
+	}
+}
+
