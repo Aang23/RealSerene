3816d2a775c913eee4054fdef889177101efeba0
Aang23 <qwerty15@gmx.fr>
2018-08-20 08:36:50 +0200

    Started subcommands implementation

---
+++ src/main/java/com/aang23/realserene/commands/InfoSubCommand.java
@@ -0,0 +1,16 @@
+package com.aang23.realserene.commands;
+
+import net.minecraft.command.CommandBase;
+import net.minecraft.command.CommandException;
+import net.minecraft.command.ICommandSender;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.TextComponentString;
+
+
+public class InfoSubCommand {
+	public void call(MinecraftServer server, ICommandSender sender, String[] args) throws CommandException {
+		
+	}
+}
